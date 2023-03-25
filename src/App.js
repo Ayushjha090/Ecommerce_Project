b8311e1293a1b2ff0dff77d05d35b71a03c91f5a
Ayush Jha <ayushjha090@gmail.com>
2023-03-25 20:21:46 +0530

    Started Ecommerce Project Using React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable require-jsdoc */
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Ecommerce React Project</h1>
+    </div>
+  );
+}
+
+export default App;
